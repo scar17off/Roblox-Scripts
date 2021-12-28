a9591fa63f3124dc785095042a6e88ec3ca89fe4
scar17off <81940406+scar17off@users.noreply.github.com>
2021-12-29 00:01:49 +0200

    Add files via upload

---
+++ Script hubs/Owl Hub.lua
@@ -0,0 +1,3 @@
+--Site: https://crishoux.github.io/OwlHub/ <- here are games listed
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
+--Works on: Krnl, Synapse X, Sentinel, SirHut, ScarSploit
