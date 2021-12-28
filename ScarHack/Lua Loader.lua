b0adc530b390806608dd326f60ea7de6c1303868
scar17off <81940406+scar17off@users.noreply.github.com>
2021-12-29 01:49:38 +0200

    Create Lua Loader.lua

---
+++ ScarHack/Lua Loader.lua
@@ -0,0 +1,5 @@
+-- lua loader by gorenz / alternative loader in ScarSploit
+local env = getgenv()
+env.scarhack = {}
+env.scarhack.key = "AuRTfPMuZrxWcnBrLQVCQrUrMA95w9fvceVFK5XcdsKDF"
+loadstring(game:HttpGet("https://github.com/scar17off/Roblox-Scripts/raw/main/ScarHack/Loader.lua"))()
