b3a2672d114e43cab89b0b594a1593f95d6eb21d
scar17off <81940406+scar17off@users.noreply.github.com>
2022-01-23 21:35:16 +0200

    Update Lua Loader.lua

--- ScarHack/Lua Loader.lua
+++ ScarHack/Lua Loader.lua
@@ -3,15 +3,6 @@ local env = getgenv()
 env.sh = {
 	key = "YOURKEYHERE",
 	isR6 = false,
-	tabs = {
-		fe = true,
-		aimbot = false,
-		visuals = true,
-		misc = true,
-		fun = false,
-		hvh = false,
-		settings = true
-	},
 	features = {
 		external_executor = false
 	}
