baa8cb2c18a8d7d862f4dd7e72b7ecaa3392e957
scar17off <81940406+scar17off@users.noreply.github.com>
2022-01-23 20:39:37 +0200

    Update Lua Loader.lua

--- ScarHack/Lua Loader.lua
+++ ScarHack/Lua Loader.lua
@@ -4,13 +4,16 @@ env.sh = {
 	key = "YOURKEYHERE",
 	isR6 = false,
 	tabs = {
-		"FE" = true,
-		"Aimbot" = false,
-		"Visuals" = true,
-		"Misc" = true,
-		"Fun" = false,
-		"HvH" = false
-		"Settings" = true
+		fe = true,
+		aimbot = false,
+		visuals = true,
+		misc = true,
+		fun = false,
+		hvh = false,
+		settings = true
+	},
+	features = {
+		external_executor = false
 	}
 }
 loadstring(game:HttpGet("https://github.com/scar17off/Roblox-Scripts/raw/main/ScarHack/ScarHack.lua"))()
